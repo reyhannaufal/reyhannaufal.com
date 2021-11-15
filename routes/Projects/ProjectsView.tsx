81fdc16353f00e2e0409b2f86a12a39c2685d369
reyhannaufal <reyhannaufal21@gmail.com>
2021-11-15 07:41:29 +0700

    feat: move projects route

---
+++ routes/Projects/ProjectsView.tsx
@@ -0,0 +1,8 @@
+export default function ProjectsView() {
+    return (
+       <div className='py-20'>
+          <h1 className='text-3xl font-bold'>Projects</h1>
+          <p>Working now!</p>
+       </div>
+    );
+}
