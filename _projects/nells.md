5e968edb0ad2080bdc349c02b392cc99e66cea53
reyhannaufal <reyhannaufal21@gmail.com>
2021-11-28 15:02:03 +0700

    feat: add projects base code for content

--- _projects/nells.md
+++ _projects/nells.md
@@ -1,10 +1,10 @@
 ---
-title: 'Preview Mode for Static Generation'
+title: 'Nells Project'
 excerpt: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Praesent elementum facilisis leo vel fringilla est ullamcorper eget. At imperdiet dui accumsan sit amet nulla facilities morbi tempus.'
 coverImage: '/assets/blog/preview/cover.jpg'
 date: '2020-03-16T05:35:07.322Z'
 author:
-   name: Joe Haddad
+   name: Reyhan Naufal Rahman
    picture: '/assets/blog/authors/joe.jpeg'
 ogImage:
    url: '/assets/blog/preview/cover.jpg'
---
+++ pages/project/[slug].tsx
@@ -0,0 +1,65 @@
+import Layout from '@/components/Layout';
+import markdownToHtml from '@/utils/markDownToHtml';
+import { getAllPosts, getPostBySlug } from '@/utils/posts';
+import { getAllProjects, getProjectBySlug } from '@/utils/projects';
+import markdownStyles from './markdown-content.module.css';
+
+export type Post = {
+   slug?: string;
+   title?: string;
+   content?: string;
+   createdAt?: string;
+   date?: string;
+};
+
+export default function Posts({ post }: { post: Post }) {
+   return (
+      <Layout>
+         <h1 className='font-bold text-3xl my-7 max-w-sm'>{post.title}</h1>
+         <div
+            className={markdownStyles['markdown']}
+            dangerouslySetInnerHTML={{ __html: post.content ?? '' }}
+         />
+      </Layout>
+   );
+}
+
+export async function getStaticProps({ params }: { params: { slug: string } }) {
+   const postFields = [
+      'slug',
+      'title',
+      'date',
+      'author',
+      'content',
+      'ogImage',
+      'coverImage',
+   ];
+   const post: Post = getProjectBySlug(params.slug, postFields as any);
+   const content = await markdownToHtml(post.content || '');
+
+   return {
+      props: {
+         post: {
+            ...post,
+            content,
+         },
+      },
+   };
+}
+
+export async function getStaticPaths() {
+   const posts = getAllProjects(['slug'] as never);
+
+   console.log(posts);
+
+   return {
+      paths: posts.map((post: any) => {
+         return {
+            params: {
+               slug: post.slug,
+            },
+         };
+      }),
+      fallback: false,
+   };
+}
