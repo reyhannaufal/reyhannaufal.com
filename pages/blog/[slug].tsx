69d38937d221119e3ad8d51af3e24cb65cc05d96
reyhannaufal <reyhannaufal21@gmail.com>
2021-12-01 21:12:32 +0700

    fix: title on mobile

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,4 +1,5 @@
 import Layout from '@/components/Layout';
+import Seo from '@/components/Layout/LayoutSeo';
 import markdownToHtml from '@/utils/markDownToHtml';
 import { getAllPosts, getPostBySlug } from '@/utils/posts';
 import markdownStyles from './markdown-content.module.css';
@@ -13,13 +14,16 @@ export type Post = {
 
 export default function Posts({ post }: { post: Post }) {
    return (
-      <Layout>
-         <h1 className='font-bold text-3xl my-7 max-w-sm'>{post.title}</h1>
-         <div
-            className={markdownStyles['markdown']}
-            dangerouslySetInnerHTML={{ __html: post.content ?? '' }}
-         />
-      </Layout>
+      <>
+         <Seo title={`Blog | ${post.title}`} />
+         <Layout>
+            <h1 className='font-bold text-xl sm:text-3xl my-7 max-w-sm'>{post.title}</h1>
+            <div
+               className={markdownStyles['markdown']}
+               dangerouslySetInnerHTML={{ __html: post.content ?? '' }}
+            />
+         </Layout>
+      </>
    );
 }
 
