98f9429d7effee9bfb24bd0efc68c2a0e84397a7
reyhannaufal <reyhannaufal21@gmail.com>
2022-01-20 22:17:34 +0700

    feat: add type

--- src/hooks/useProgressStore.ts
+++ src/hooks/useProgressStore.ts
@@ -1,6 +1,5 @@
 import create from 'zustand';
 
-
 export const useProgressStore = create((set) => ({
    isAnimating: false,
    setIsAnimating: (isAnimating: boolean) => set(() => ({ isAnimating })),
