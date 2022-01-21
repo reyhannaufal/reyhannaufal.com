5130690e3e68b9d71a4f97625e946eead438d1ac
reyhannaufal <reyhannaufal21@gmail.com>
2022-01-21 13:54:24 +0700

    feat: add projects and post type

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,102 @@
 import type { NextPage } from 'next';
+import Image from 'next/image';
+import {
+   SiJavascript,
+   SiNextDotJs,
+   SiReact,
+   SiRedux,
+   SiTypescript,
+} from 'react-icons/si';
 
-import { getAllProjects } from '@/src/utils/projects';
+import CardView from '@/src/components/Card/CardView';
 import Layout from '@/src/components/Layout';
 import Seo from '@/src/components/Layout/LayoutSeo';
-import HomeView from '@/src/routes/Home/HomeView';
+import { getAllProjects } from '@/src/utils/projects';
+import { Post } from '../constants/posts';
 
-const Home: NextPage = ({ allPosts }: any) => {
-   return (
-      <Layout>
-         <Seo />
-         <HomeView posts={allPosts} />
-      </Layout>
-   );
+const stackStyles = {
+   styles:
+      'w-7 h-7 text-primary hover:text-secondary hover:scale transition  ease-in-out transform hover:-translate-y-1 hover:scale-110',
 };
 
+interface HomeViewProps {
+   posts: Post[];
+}
+
+const Home = ({ posts }: HomeViewProps) => (
+   <Layout>
+      <Seo />
+      <section className='flex flex-col-reverse items-center py-4 sm:py-14 lg:flex-row'>
+         <div className='max-w-lg text-left'>
+            <div className='space-y-1 md:space-y-2'>
+               <p className='mt-2 text-sm font-bold lg:mt-0'>Hey, I'm</p>
+               <h1 className='max-w-[300px] md:max-w-[500px] lg:max-w-sm  text-left lg:mx-0 text-2xl md:text-3xl font-bold  lg:text-4xl text-primary'>
+                  Reyhan Naufal Rahman
+               </h1>
+               <h2 className='text-lg font-bold md:text-2xl text-secondary'>
+                  Frontend Engineer
+               </h2>
+               <p className='text-base font-normal text-coolGray'>
+                  Reyhan has experience of writting web apps. He has a knowledge
+                  of Javascript, Typescript and browser APIS as well as
+                  significant experience with popular libraries like React and
+                  Redux.{' '}
+               </p>
+            </div>
+            <div className='flex justify-start mx-auto mt-8 space-x-6'>
+               <SiJavascript className={stackStyles.styles} />
+               <SiTypescript className={stackStyles.styles} />
+               <SiReact className={stackStyles.styles} />
+               <SiNextDotJs className={stackStyles.styles} />
+               <SiRedux className={stackStyles.styles} />
+            </div>
+         </div>
+         <div className='mx-auto mt-20 lg:mt-0'>
+            <div className='w-52 h-52 md:h-72 md:w-72 lg:h-auto lg:w-auto'>
+               <Image
+                  src='/laptop-illustrations.svg'
+                  placeholder='blur'
+                  height={441.64}
+                  width={519}
+                  alt='laptop-illusrations'
+                  blurDataURL='/laptop-illustrations.svg'
+               />
+            </div>
+         </div>
+      </section>
+
+      <section className='pt-10 pb-5 '>
+         <div className='mt-20 space-y-1 text-center lg:mt-10 lg:text-left'>
+            <h1 className='text-3xl font-bold lg:text-4xl text-primary'>
+               Notable Projects
+            </h1>
+            <p className='text-base font-normal'>
+               Some cool projects that I have worked on!
+            </p>
+         </div>
+
+         <div className='flex flex-col sm:flex-row sm:space-x-5'>
+            {posts?.map((item: any) => (
+               <CardView item={item} isProjectCard />
+            ))}
+         </div>
+      </section>
+   </Layout>
+);
+
 export async function getStaticProps() {
-   const allPosts = getAllProjects([
-      'title' as never,
-      'date' as never,
-      'slug' as never,
-      'author' as never,
-      'coverImage' as never,
-      'excerpt' as never,
-      'type' as never,
-   ]);
+   const posts = getAllProjects([
+      'title',
+      'date',
+      'slug',
+      'author',
+      'coverImage',
+      'excerpt',
+      'type',
+   ] as never);
 
    return {
-      props: { allPosts },
+      props: { posts },
    };
 }
 
