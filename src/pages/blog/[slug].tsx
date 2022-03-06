0a4c920ce40fb3b931e6be96976074c543a9f22c
reyhannaufal <reyhannaufal21@gmail.com>
2022-03-06 15:47:16 +0700

    feat: handle fallback route static path issue

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -30,16 +30,16 @@ export default function BlogBySlug({
             <div className='flex text-xs sm:text-base mt-2 mb-4 space-x-2 text-gray-500'>
                <p>{frontMatter?.author?.name}</p>
                <p>-</p>
-               <p>{frontMatter.date}</p>
+               <p>{frontMatter?.date}</p>
             </div>
             <Image
                className='rounded-lg w-full h-[250px] object-cover sm:h-[500px]'
-               src={frontMatter.coverImage as string}
+               src={frontMatter?.coverImage as string}
                placeholder='blur'
-               blurDataURL={frontMatter.coverImage}
+               blurDataURL={frontMatter?.coverImage}
                height={500}
                width={955}
-               alt={frontMatter.title}
+               alt={frontMatter?.title}
             />
             <section className='prose'>
                <MDXRemote {...source} />
@@ -49,13 +49,13 @@ export default function BlogBySlug({
    );
 }
 
-export async function getStaticPaths() {
+export function getStaticPaths() {
    const projectsDirectory = path.join(process.cwd(), 'src/data/projects');
    const filenames = fs.readdirSync(projectsDirectory);
    const paths = filenames.map((name) => ({
       params: { slug: name.replace('.mdx', '') },
    }));
-   return { paths, fallback: true };
+   return { paths, fallback: false };
 }
 export async function getStaticProps({ params }: { params: { slug: string } }) {
    const projectsPath = path.join(
