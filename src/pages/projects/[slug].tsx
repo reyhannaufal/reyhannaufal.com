e9ece408b28a87928156feaae4cc657551e17cac
reyhannaufal <reyhannaufal21@gmail.com>
2022-03-06 14:49:02 +0700

    feat: migrate projects file to mdx

--- src/pages/projects/[slug].tsx
+++ src/pages/projects/[slug].tsx
@@ -1,86 +1,71 @@
+import path from 'path';
+import fs from 'fs';
+import matter from 'gray-matter';
 import React from 'react';
 import Image from 'next/image';
+import { serialize } from 'next-mdx-remote/serialize';
+import { MDXRemote } from 'next-mdx-remote';
 
 import Layout from '@/src/components/Layout';
 import Seo from '@/src/components/Layout/LayoutSeo';
-import markdownToHtml from '@/src/utils/markDownToHtml';
-import { getAllMdxFiles, getMdxFileBySlug } from '@/src/utils/mdx';
-import markdownStyles from './markdown-content.module.css';
-import { Post } from '@/src/constants/posts';
-import { Project } from '@/src/constants/projects';
 
 export interface ProjectBySlugViewProps {
-   post: Post;
+   source: any;
+   frontMatter: any;
 }
 
-export default function ProjectBySlug({ post }: ProjectBySlugViewProps) {
+export default function ProjectBySlug({
+   source,
+   frontMatter,
+}: ProjectBySlugViewProps) {
+   console.log(source);
    return (
       <>
-         <Seo title={`Projects | ${post.title}`} />
+         <Seo title={`Projects | ${frontMatter.title}`} />
          <Layout>
-            <h1 className='font-bold text-xl sm:text-3xl mt-7'>{post.title}</h1>
+            <h1 className='font-bold text-xl sm:text-3xl mt-7'>
+               {frontMatter?.title}
+            </h1>
             <div className='flex text-xs sm:text-base mt-2 mb-4 space-x-2 text-gray-500'>
-               <p>{post?.author?.name}</p>
+               <p>{frontMatter?.author?.name}</p>
                <p>-</p>
-               <p>{post.date}</p>
+               <p>{frontMatter?.date}</p>
             </div>
             <Image
                className='w-full h-[350px] object-cover sm:h-[500px]'
-               src={post.coverImage as string}
+               src={frontMatter?.coverImage as string}
                placeholder='blur'
-               blurDataURL={post.coverImage}
+               blurDataURL={frontMatter?.coverImage}
                height={500}
                width={955}
-               alt={post.title}
+               alt={frontMatter?.title}
             />
-            <div
-               className={markdownStyles['markdown']}
-               dangerouslySetInnerHTML={{ __html: post.content ?? '' }}
-            />
+            <section className='prose'>
+               <MDXRemote {...source} />
+            </section>
          </Layout>
       </>
    );
 }
 
 export async function getStaticProps({ params }: { params: { slug: string } }) {
-   const postFields = [
-      'slug',
-      'title',
-      'date',
-      'author',
-      'content',
-      'ogImage',
-      'coverImage',
-   ];
-
-   const post: Project = getMdxFileBySlug(
-      params.slug,
-      postFields as [],
-      'src/data/projects'
+   const projectsPath = path.join(
+      process.cwd(),
+      'src/data/projects',
+      `${params.slug}.mdx`
    );
-   const content = await markdownToHtml(post.content || '');
-
-   return {
-      props: {
-         post: {
-            ...post,
-            content,
-         },
-      },
-   };
+   const projectsSource = fs.readFileSync(projectsPath, 'utf8');
+   const { content, data } = matter(projectsSource);
+   const mdxSource = await serialize(content);
+   return { props: { source: mdxSource, frontMatter: data } };
 }
 
 export async function getStaticPaths() {
-   const projects = getAllMdxFiles(['slug'] as never, 'src/data/projects');
-
-   return {
-      paths: projects.map((project: Project) => {
-         return {
-            params: {
-               slug: project.slug,
-            },
-         };
-      }),
-      fallback: false,
-   };
+   const projectsDirectory = path.join(process.cwd(), 'src/data/projects');
+   const filenames = fs.readdirSync(projectsDirectory);
+   const paths = filenames.map((name) => ({
+      params: { slug: name.replace('.mdx', '') },
+   }));
+   console.log('paths', paths);
+   return { paths, fallback: true };
 }
