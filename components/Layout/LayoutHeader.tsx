2a70df032004c096fbd4f7db84c2d7114188e8f6
reyhannaufal <reyhannaufal21@gmail.com>
2022-07-14 16:29:26 +0700

    feat: update header

--- components/Layout/LayoutHeader.tsx
+++ components/Layout/LayoutHeader.tsx
@@ -1,53 +1,142 @@
-import React from 'react';
-import { useRouter } from 'next/router';
+import React, { Fragment } from 'react';
+import { Popover, Transition } from '@headlessui/react';
 
 import Image from 'next/image';
 import Link from 'next/link';
-import { classNames } from '@/utils/classNames';
+import clsx from 'clsx';
+import ButtonLink from './ButtonLink';
 
-const headerList = [
-   { id: '1', title: 'Home', href: '/' },
-   { id: '2', title: 'Blog', href: '/blog' },
-   { id: '2', title: 'Projects', href: '/projects' },
-];
+// const headerList = [{ id: '1', title: 'Home', href: '/' }];
+
+function MobileNavigation() {
+   return (
+      <Popover>
+         {({ open, close }) => (
+            <>
+               <Popover.Button className='[&:not(:focus-visible)]:focus:outline-none relative z-10 flex h-8 w-8 items-center justify-center'>
+                  <span className='sr-only'>Toggle Navigation</span>
+                  <svg
+                     aria-hidden='true'
+                     className='h-3.5 w-3.5 overflow-visible stroke-slate-700'
+                     fill='none'
+                     strokeWidth={2}
+                     strokeLinecap='round'
+                  >
+                     <path
+                        d='M0 1H14M0 7H14M0 13H14'
+                        className={clsx('origin-center transition', {
+                           'scale-90 opacity-0': open,
+                        })}
+                     />
+                     <path
+                        d='M2 2L12 12M12 2L2 12'
+                        className={clsx('origin-center transition', {
+                           'scale-90 opacity-0': !open,
+                        })}
+                     />
+                  </svg>
+               </Popover.Button>
+               <Transition.Root>
+                  <Transition.Child
+                     as={Fragment}
+                     enter='duration-150 ease-out'
+                     enterFrom='opacity-0'
+                     enterTo='opacity-100'
+                     leave='duration-150 ease-in'
+                     leaveFrom='opacity-100'
+                     leaveTo='opacity-0'
+                  >
+                     <Popover.Overlay className='fixed inset-0 bg-slate-300/50' />
+                  </Transition.Child>
+                  <Transition.Child
+                     as={Fragment}
+                     enter='duration-150 ease-out'
+                     enterFrom='opacity-0 scale-95'
+                     enterTo='opacity-100 scale-100'
+                     leave='duration-100 ease-in'
+                     leaveFrom='opacity-100 scale-100'
+                     leaveTo='opacity-0 scale-95'
+                  >
+                     <Popover.Panel
+                        as='ul'
+                        className='absolute inset-x-0 top-full mt-4 origin-top space-y-4 rounded-2xl bg-white p-6 text-lg tracking-tight text-slate-900 shadow-xl ring-1 ring-slate-900/5'
+                     >
+                        <li>
+                           <Link href='/blog'>
+                              <a
+                                 className='block w-full'
+                                 onClick={() => close()}
+                              >
+                                 Blog
+                              </a>
+                           </Link>
+                        </li>
+                        <li>
+                           <Link href='/projects'>
+                              <a
+                                 className='block w-full'
+                                 onClick={() => close()}
+                              >
+                                 Projects
+                              </a>
+                           </Link>
+                        </li>
+                     </Popover.Panel>
+                  </Transition.Child>
+               </Transition.Root>
+            </>
+         )}
+      </Popover>
+   );
+}
 
 export default function Header() {
-   const router = useRouter();
-   const { pathname } = router;
-   const isActiveLink = (href: string) => {
-      if (href === '/projects' || href === '/blog') {
-         return router.pathname?.startsWith(href) ? 'active' : '';
-      }
-      return pathname === href ? 'active' : '';
-   };
-
    return (
-      <header className='flex items-center justify-between py-5'>
-         <div className='h-7 w-7 md:h-auto md:w-auto'>
-            <Link href='/' passHref>
-               <Image
-                  src='/reyhan-logo.svg'
-                  alt='logo'
-                  width={44.17}
-                  height={43.91}
-               />
-            </Link>
-         </div>
-         <div className='flex items-center space-x-4 text-xs font-medium text-gray-400 sm:text-sm'>
-            {headerList.map((item) => (
-               <div
-                  key={item.title}
-                  className={classNames(
-                     'flex justify-center rounded-md px-3 py-2 hover:text-gray-300 sm:px-3',
-                     isActiveLink(item.href)
-                        ? 'bg-primary text-gray-100 hover:text-gray-100'
-                        : ''
-                  )}
-               >
-                  <Link href={item.href}>{item.title}</Link>
-               </div>
-            ))}
-         </div>
+      <header className='py-10'>
+         <nav className='relative z-50 text-sm'>
+            <ul className='flex items-center'>
+               <li>
+                  <Link href='/'>
+                     <a>
+                        <span className='sr-only'>Home</span>
+                        <div className='h-7 w-7 md:h-8 md:w-8'>
+                           <Image
+                              src='/reyhan-logo.svg'
+                              alt='logo'
+                              width={44.17}
+                              height={43.91}
+                           />
+                        </div>
+                     </a>
+                  </Link>
+               </li>
+               {/* TODO: Improve header */}
+               {/* {headerList.map((item) => (
+                  <li key={item.title} className='ml-12 hidden md:block'>
+                     <Link href={item.href}>
+                        <a className='rounded-lg py-1 px-2 text-slate-700 hover:bg-slate-200 hover:text-slate-900'>
+                           {item.title}
+                        </a>
+                     </Link>
+                  </li>
+               ))} */}
+               <li className='ml-auto hidden md:block'>
+                  <Link href='/blog'>
+                     <a className='rounded-lg py-1 px-2 text-slate-700 hover:bg-slate-100 hover:text-slate-900'>
+                        Blog
+                     </a>
+                  </Link>
+               </li>
+               <li className='ml-auto md:ml-8'>
+                  <ButtonLink href='/projects' color='blue'>
+                     <span>Projects</span>
+                  </ButtonLink>
+               </li>
+               <li className='ml-5 -mr-1 md:hidden'>
+                  <MobileNavigation />
+               </li>
+            </ul>
+         </nav>
       </header>
    );
 }
