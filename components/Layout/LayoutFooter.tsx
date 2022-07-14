83e70e54f52e992daa0cbbeab3e6db7afdd04575
reyhannaufal <reyhannaufal21@gmail.com>
2022-07-14 15:59:33 +0700

    feat: update footer design

--- components/Layout/LayoutFooter.tsx
+++ components/Layout/LayoutFooter.tsx
@@ -1,39 +1,27 @@
 import React from 'react';
 import { IoLogoGithub, IoLogoLinkedin } from 'react-icons/io';
 
-const footerLogoSize = 'w-7 h-7';
-const footerList = [
-   {
-      id: '1',
-      icon: <IoLogoGithub className={footerLogoSize} />,
-      link: 'https://github.com/reyhannaufal',
-   },
-   {
-      id: '2',
-      icon: <IoLogoLinkedin className={footerLogoSize} />,
-      link: 'https://www.linkedin.com/in/reyhan-naufal/',
-   },
-];
+const footerLogoSize = 'w-5 h-5 text-slate-400 hover:text-slate-500';
 
 export default function Footer() {
    return (
-      <footer className='mx-auto flex-col py-20 text-center text-primary sm:pt-36'>
-         <h2 className='mx-auto max-w-3xl rounded-sm border-t-2 py-4 text-sm font-bold sm:text-lg '>
-            Contact Me
-         </h2>
-         <div className='flex flex-row justify-center space-x-2 pb-3'>
-            {footerList.map((item) => (
-               <a
-                  key={item.id}
-                  href={item.link}
-                  rel='noopener noreferrer '
-                  target='_blank'
-               >
-                  {item.icon}
+      <footer>
+         <div className='flex flex-col items-center border-t border-slate-400/10 py-10 sm:flex-row-reverse sm:justify-between'>
+            <div className='flex items-start space-x-6'>
+               <a className='group'>
+                  <span className='sr-only'>Reyhan on Linkedin</span>
+                  <IoLogoLinkedin className={footerLogoSize} />
                </a>
-            ))}
+               <a className='group'>
+                  <span className='sr-only'>Reyhan on GitHub</span>
+                  <IoLogoGithub className={footerLogoSize} />
+               </a>
+            </div>
+            <p className='mt-6 text-sm text-slate-500 sm:mt-0'>
+               Copyright &copy; {new Date().getFullYear()} Reyhan Naufal Rahman.
+               All rights reserved.
+            </p>
          </div>
-         <p className='text-sm md:text-base'>© Reyhan Naufal Rahman 2021 </p>
       </footer>
    );
 }
